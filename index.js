a0c20389be31ea2add4ac8a641c466d500d2446a
Dustinlafontaine <zzzdustzzz@gmail.com>
2023-04-28 11:02:46 -0500

    New Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let partone = "the gold";
+
+
+let parttwo = "is below";
+
+let partthree = "the castle.";
+
+function decryption(){
+    return partone + ' '+ parttwo + ' ' + partthree + ' '
+}
+
+console.log(decryption())
